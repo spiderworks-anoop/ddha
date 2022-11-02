a951c46ae3e00cd071af0b77edf862250bb52f65
spiderworks-anoop <anoop@spiderworks.in>
2022-11-02 22:55:47 +0530

    changes

--- resources/views/client/ui/base.blade.php
+++ resources/views/client/ui/base.blade.php
@@ -7,7 +7,7 @@
    <meta charset="utf-8">
 
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <title>Best International School in Thrissur | IGCSE, CBSE, IB School Kerala </title>
+   <title>Dehradun Hills Academy </title>
    @include('client.ui.include.style')
 
    @section('head')
